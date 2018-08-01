570b7492f9cacf2a932742af968c721cce93d656
Aritra-Ghosal <40317399+Aritra-Ghosal@users.noreply.github.com>
2018-08-01 18:10:05 +0530

    Add files via upload

---
+++ Assignment2_2_Q3.R
@@ -0,0 +1,20 @@
+m=10
+n=rnorm(10)
+x=data.frame(replicate(m,rnorm(n)))
+d=x
+t1=Sys.time()
+for(i in seq(nrow(x))){
+  for(j in seq(nrow(x))){
+    d[i,j]=round(d[i,j]+sin(1.570796),3)
+  }
+}
+t2=Sys.time()
+print(paste0("For loops takes", round((t2-t1),5),"second"))
+z=x
+t3=Sys.time()
+y=sapply(z,function(k){k=round(k+sin(1.570796),3)})
+t4=Sys.time()
+print(paste0("for loop takes",round((t4-t3),5),"seconds"))
+x[1,]
+y[1,]
+d[1,]
